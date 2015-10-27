fb1106acda2db57bcbb5021f9b70b90fd4d9ce74
Adam Ruzicka <aruzicka@redhat.com>
2015-10-27 15:25:03 +0100

    Refs #10755 - UI changes

--- app/models/job_invocation_task_group.rb
+++ app/models/job_invocation_task_group.rb
@@ -2,4 +2,10 @@ class JobInvocationTaskGroup < ::ForemanTasks::TaskGroup
 
   has_one :job_invocation, :foreign_key => :task_group_id
 
+  alias_method :associated_resource, :job_invocation
+
+  def resource_name
+    N_('Job Invocation')
+  end
+
 end
